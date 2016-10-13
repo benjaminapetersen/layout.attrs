4bc8e28264fdf46be3531f67b21e8b4564664533
benjaminapetersen <admin@benjaminapetersen.me>
2016-10-13 13:27:59 -0400

    Prototype of new generator of both classes & attrs

--- gulpfile.js
+++ gulpfile.js
@@ -6,16 +6,32 @@
       filesize = require('gulp-filesize'),
       // dest = require('gulp-dest'), https://www.npmjs.com/package/gulp-dest
       sass = require('gulp-sass'),
-      sourcemaps = require('gulp-sourcemaps');
+      sourcemaps = require('gulp-sourcemaps'),
+      //minifycss = require('gulp-clean-css'),
+      autoprefixer = require('gulp-autoprefixer'),
+      //uglify = require('gulp-uglify'),
+      //rename = require('gulp-rename'),
+      //concat = require('gulp-concat'),
+      del = require('del');
+      //browserSync = require('browser-sync'),
+      //reload = browserSync.reload,
+      //path = require('path'),
+      //shell = require('gulp-shell');
+
 
   var match = {
     recurse: '**/*'
   };
 
-  var src = './src/';
-  var dist = './dist/';
+  var root = './',
+      src = './src/',
+      tmp = './.tmp/',
+      tmpBuild = tmp + 'build/',
+      dist = './dist/';
 
   var srcAll = src + match.recurse,
+      tmpAll = tmpBuild + match.recurse,
+      distAll = dist + match.recurse,
       classesAll = srcAll + 'classes.scss',
       attrsAll = srcAll + 'attrs.scss',
       distClasses = dist + 'classes',
@@ -27,7 +43,13 @@
     outputStyle: 'expanded'
   };
 
-  gulp.task('sass-classes', function() {
+  gulp.task('clean', function() {
+    return del([distAll, tmpAll, './dist_new/'], function(err, paths) {
+      return gutil.log('cleaned files/folders:\n', paths.join('\n'), gutil.colors.green());
+    });
+  });
+
+  gulp.task('sass-classes', ['clean'], function() {
     return gulp
       .src(classesAll)
       .pipe(sass(sassConf).on('error', sass.logError))
@@ -35,7 +57,7 @@
       .pipe(filesize());
   });
 
-  gulp.task('sass-attrs', function() {
+  gulp.task('sass-attrs', ['clean'], function() {
     return gulp
       .src(attrsAll)
       .pipe(sass(sassConf).on('error', sass.logError))
@@ -43,14 +65,17 @@
       .pipe(filesize());
   });
 
-  // gulp.task('sass', function () {
-  //   return gulp
-  //     .src(srcSass)
-  //     .pipe(sass())
-  //     .pipe(gulp.dest(dist))
-  //     .pipe(filesize());
-  // });
-  gulp.task('sass-all', ['sass-classes', 'sass-attrs'])
+  gulp.task('sass-test', function () {
+    return gulp
+      .src('./src-new/**/*')
+      .pipe(sass())
+      // .pipe(autoprefixer('last 10 versions'))
+      // .pipe(minifycss()) prefer to do a separate min run.
+      .pipe(gulp.dest('./dist-new'))
+      .pipe(filesize());
+  });
+
+  gulp.task('sass-all', ['sass-classes', 'sass-attrs']);
 
   gulp.task('default', ['sass-all']);
 
